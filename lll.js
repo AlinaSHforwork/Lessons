d7ba675a1aecd4c296bba3b18bec4c0d3bef3483
Alina💕 <alinashforwork@gmail.com>
2025-09-30 10:06:19 +0300

    Update lll.js
    Xycug

--- lll.js
+++ lll.js
@@ -1,10 +1,22 @@
-const aArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numArr = []
+   for (let i = 0; i < 6000000; i++) {
+      numArr[i] = Math.round(Math.random() * 100 - 50);
+   }
+   console.time(sorting)
+   
 
-console.table(aArray);
-let indexi = Math.round(aArray.length / 2) - 1
-let indexj = indexi + 1
-
-while (indexi >= 0 && indexj < aArray.length) {
-  //console.log(aArray[index]);
-  console.log(aArray[indexi--], aArray[indexj++]);
-}
+   console.log(numArr)
+   let i = 1
+   for(let star = 0; star < numArr.length -1 ; star++){
+  for (let i = star + 1 ; i < numArr.length; i++) {
+     if (numArr[star] >= numArr [i]){
+         let temp = numArr[star] 
+         numArr[star] = numArr[i]
+         numArr[i] = temp
+         
+     }
+   }
+  }
+   
+//console.log(numArr)
+console.timeEnd(sorting)
