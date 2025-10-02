90ef2db22ec89111534078488d68cb7dcfa64013
AlinaSHforwork <alinashforwork@gmail.com>
2025-10-02 18:34:28 +0300

    update 3009

---
+++ 3009.js
@@ -0,0 +1,23 @@
+console.log("Lesson about functions")
+
+function square(x) {
+return x * x
+}
+
+function cub(x) { 
+    return x * x * x
+}
+
+function doMath(functionToExecute, param) { 
+    return functionToExecute(param)
+}
+
+const a = square(3)
+const b = doMath(square, 3)
+const c = doMath(cub, 3)
+
+console.log("Number in square:", a) 
+console.log("Number in square:",b) 
+console.log("Number in cub:",c)
+
+console.log("Katya is Cocacolic (>'^'<)")
--- lll.js
+++ lll.js
@@ -1,10 +1,27 @@
-const aArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-console.table(aArray);
-let indexi = Math.round(aArray.length / 2) - 1
-let indexj = indexi + 1
-
-while (indexi >= 0 && indexj < aArray.length) {
-  //console.log(aArray[index]);
-  console.log(aArray[indexi--], aArray[indexj++]);
+let numArr = []
+for (let i = 0; i < 10; i++) {
+  numArr[i] = Math.round (Math.random() * 100 - 50);
 }
+console.log("numArr array:",numArr)
+
+
+let meineArr = [5, 3, -8, 1, -2]
+console.log("meineArr array:",meineArr)
+
+let i = 1
+
+function sorting (ARAY){
+  for(let star = 0; star < ARAY.length -1; star++){
+  for (let i = star + 1; i < ARAY.length; i++) {
+    if (ARAY[star] >= ARAY [i]){
+      let temp = ARAY[star]
+      ARAY[star] = ARAY[i]
+      ARAY[i] = temp
+    }
+  }
+}
+return ARAY
+}
+
+console.log("sorted numArr array:",sorting(numArr))
+console.log("sorted meineArr array:",sorting(meineArr))
