a49fe1fadf67982f453d9cb36ab5a7b5d3f7c0fa
AlinaSHforwork <alinashforwork@gmail.com>
2025-10-02 18:35:42 +0300

    up homework

--- lll.js
+++ lll.js
@@ -1,5 +1,4 @@
 let numArr = []
-<<<<<<< HEAD
 for (let i = 0; i < 10; i++) {
   numArr[i] = Math.round (Math.random() * 100 - 50);
 }
@@ -26,26 +25,3 @@ return ARAY
 
 console.log("sorted numArr array:",sorting(numArr))
 console.log("sorted meineArr array:",sorting(meineArr))
-=======
-   for (let i = 0; i < 6000000; i++) {
-      numArr[i] = Math.round(Math.random() * 100 - 50);
-   }
-   console.time(sorting)
-   
-
-   console.log(numArr)
-   let i = 1
-   for(let star = 0; star < numArr.length -1 ; star++){
-  for (let i = star + 1 ; i < numArr.length; i++) {
-     if (numArr[star] >= numArr [i]){
-         let temp = numArr[star] 
-         numArr[star] = numArr[i]
-         numArr[i] = temp
-         
-     }
-   }
-  }
-   
-//console.log(numArr)
-console.timeEnd(sorting)
->>>>>>> d7ba675a1aecd4c296bba3b18bec4c0d3bef3483
