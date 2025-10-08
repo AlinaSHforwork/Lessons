7ac3f8ae6ff9c47b4624c5a79a7dd48582edb5ea
AlinaSHforwork <alinashforwork@gmail.com>
2025-10-08 09:08:10 +0300

    homework

--- 0710.js
+++ 0710.js
@@ -1,4 +1,4 @@
-const toFillter = [-1, 3, 0, null, undefined, "5", 7, -5, 2,"7738ggf", 4, -2, "huefhe837877", -3, 8, -7]
+const toFillter = [-1, 3, 0, null, undefined, "5", 7,"katya", -5, 2,"7738ggf", 4, -2, "ijq34", -3, 8,"cat", -7]
 
 function filtrovane(arr = []) {
     if (!Array.isArray(arr)) {
@@ -6,16 +6,28 @@ function filtrovane(arr = []) {
     } 
     else
     console.log("original array:",arr)
-    let result = []
+    let resultNum = []
+    let resultStr = []
+    let resultNull = []
+    let resultUndefined = []
     for (let i = 0; i < arr.length; i++) {
         if (typeof arr[i] === "number") {
-            result.push(arr[i])
+            resultNum.push(arr[i])
         }
         if (typeof arr[i] === "string" && !isNaN(parseFloat(arr[i]))) {
-            result.push(parseFloat(arr[i]))
+            resultNum.push(parseFloat(arr[i]))
+        }
+        if (typeof arr[i] === "string" && isNaN(parseFloat(arr[i]))) {
+            resultStr.push(arr[i])
+        }
+        if (arr[i] === null) {
+            resultNull.push(arr[i])
+        }
+        if (arr[i] === undefined) {
+            resultUndefined.push(arr[i])
         }
     }
-    return result
+    return ["filtrovane numbers =>",resultNum, "filtrovane string =>" ,resultStr, "filtrovane null =>", resultNull, "filtrovane undefined =>", resultUndefined]
 }
 
 const a = filtrovane(toFillter);
@@ -23,7 +35,7 @@ console.log("filtrovane array:",a)
 
 console.log("----------------------------------------")
 
-const par1 = parseInt("100",10)
-const par2 = parseFloat("12.34",10)
-console.log("par1:",par1)
-console.log("par2:",par2)
+// const par1 = parseInt("100",10)
+// const par2 = parseFloat("12.34",10)
+// console.log("par1:",par1)
+// console.log("par2:",par2)
