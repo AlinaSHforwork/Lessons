603df9ba1710b104b41b2ab2ec16c8f5a06d5eab
AlinaSHforwork <alinashforwork@gmail.com>
2025-10-10 10:06:55 +0300

    1010

---
+++ 1010.js
@@ -0,0 +1,36 @@
+const sortedArr = [0, 3, 5, 8, 9, 10, 30, 41, 44, 45, 70, 100, 102]
+
+    /**
+     * 
+     * @param {Array} arr
+     * @param {number} value
+     * @returns {number | null}
+     */
+
+function binarySearch(arr, value) {
+    let midIndex = Math.round(arr.length / 2)
+    let startIndex = 0
+    let endIndex = arr.length
+    console.log (startIndex, endIndex, startIndex !== endIndex)
+        
+    while ((startIndex !== endIndex) + 1){
+        if (value === arr[midIndex]){
+            return arr[midIndex]
+        }
+        
+        if (value < arr[midIndex]){
+    // from 0 to midIndex - 1
+            endIndex = midIndex - 1
+            midIndex = Math.round((startIndex + endIndex) / 2)
+        }
+
+        if (value > arr[midIndex]){
+    // from midIndex + 1 to arr.length
+            startIndex = midIndex + 1
+            midIndex = Math.round((startIndex + endIndex) / 2)
+        }
+        console.log(midIndex, startIndex, endIndex)
+    }
+}
+const a = binarySearch(sortedArr, 9)
+console.log(a)
