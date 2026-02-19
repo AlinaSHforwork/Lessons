ab28361a1dbbe938b08d410380558953214ffaf0
AlinaSHforwork <alinashforwork@gmail.com>
2026-02-19 14:28:21 +0200

    add two more tests

--- count-and-tests/main.test.js
+++ count-and-tests/main.test.js
@@ -1,4 +1,5 @@
 import { jest } from '@jest/globals';
+import fs from 'node:fs';
 
 let inputs = [];
 
@@ -51,14 +52,42 @@ describe('App Tests', () => {
 
     await startApp();
 
-    process.stdin.emit('data', Buffer.from('Hello world\n'));
+    process.stdin.emit('data', Buffer.from('Hello world from pandas and fire!\n'));
     process.stdin.emit('end');
 
     expect(console.log).toHaveBeenCalledWith(
       expect.stringContaining('Total Symbols (non-whitespace):')
     );
     expect(console.log).toHaveBeenCalledWith(
-      expect.stringContaining('10')
+      expect.stringContaining('28')
     );
+  });
+
+  test('should count words correctly with text file', async () => {
+    const testText = fs.readFileSync('./text.txt', 'utf8');
+    mockUserInput(['n', 'y', 'n']);
+
+    await startApp();
+
+    process.stdin.emit('data', Buffer.from(testText));
+    process.stdin.emit('end');
+
+    expect(console.log).toHaveBeenCalledWith('Total Words: 15');
+    expect(console.log).toHaveBeenCalledWith('Unique Words:', 12);
+    expect(console.log).toHaveBeenCalledWith(
+      expect.stringContaining('Words:')
+    );
+  });
+
+  test('should count sentences correctly with text file', async () => {
+    const testText = fs.readFileSync('./text.txt', 'utf8');
+    mockUserInput(['n', 'n', 'y']);
+
+    await startApp();
+
+    process.stdin.emit('data', Buffer.from(testText));
+    process.stdin.emit('end');
+
+    expect(console.log).toHaveBeenCalledWith('Total Sentences: 9');
   });
 });
